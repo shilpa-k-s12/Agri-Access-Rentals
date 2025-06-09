307e509b08429326a218b30e49f02bb0ae7a9953
shilpa-k-s12 <shilpashama510@gmail.com>
2025-06-09 13:56:03 +0530

    Update dbconnection.php

--- admin/includes/dbconnection.php
+++ admin/includes/dbconnection.php
@@ -1,7 +1,7 @@
-<?php
-$con=mysqli_connect("localhost", "root", "", "aermsdb");
-if(mysqli_connect_errno()){
-echo "Connection Fail".mysqli_connect_error();
-}
-
-  ?>
+$con = mysqli_connect(
+    "shortline.proxy.rlwy.net", // Host
+    "root",                     // Username
+    "jXnqIAPTrAokFkzTaQeEuZUzesgaobaa", // Password
+    "railway",                  // Database
+    13306                       // Port
+);
